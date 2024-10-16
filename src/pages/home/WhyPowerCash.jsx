0ac224a2dfc912dd58a264d7f1ef0de2fbb24ce2
VishalPowerCash <vishal.powercash@gmail.com>
2024-10-16 09:28:23 +0530

    v1.0

---
+++ src/pages/home/WhyPowerCash.jsx
@@ -0,0 +1,56 @@
+export default function WhyPowerCash() {
+    return (
+        <div className='w-100 py-1 ps-3 ps-md-4 bg-black m-0'>
+            <div className="row bg-black text-white w-100 col-md-8 p-3  p-md-5">
+                <div className="col-12 col-md-6 text-start d-flex align-items-center flex-wrap">
+                    <div className="my-3">
+                        <h1 className="mb-3">Why PowerCash? </h1>
+                        <p className="" style={{ color: "#ffffffaa" }}>At PowerCash, we redefine
+                            business loans with key features</p>
+                    </div>
+                </div>
+
+                <div className="row col-12 col-md-6">
+                    <div className="col-6 col-lg-4 text-start p-3">
+                        <img src="WhyPowerCash/lFast.svg" className="m-1 mb-3 img-fluid" style={{ width: "35px" }} alt="" />
+                        <h6 className="display-6" style={{ fontSize: "0.9rem" }}>Lightning-Fast Approvals</h6>
+                        <p className="display-6" style={{ fontSize: "0.6rem" }}>Get your funds disbursed in 2-4
+                            days</p>
+                    </div>
+                    <div className="col-6 col-lg-4 text-start p-3">
+                        <img src="WhyPowerCash/easyApp.svg" className="m-1 mb-3 img-fluid" style={{ width: "35px" }} alt="" />
+                        <h6 className="display-6" style={{ fontSize: "0.9rem" }}>Easy Loan Application</h6>
+                        <p className="display-6" style={{ fontSize: "0.6rem" }}>Apply within 5 minutes with minimal
+                            documentation</p>
+                    </div>
+                    <div className="col-6 col-lg-4 text-start p-3">
+                        <img src="WhyPowerCash/flexible.svg" className="m-1 mb-3 img-fluid" style={{ width: "35px" }} alt="" />
+                        <h6 className="display-6" style={{ fontSize: "0.9rem" }}>Flexible Repayment Plans</h6>
+                        <p className="display-6" style={{ fontSize: "0.6rem" }}>Simple repayment options to suit
+                            your business</p>
+                    </div>
+                    <div className="col-6 col-lg-4 text-start p-3">
+                        <img src="WhyPowerCash/hasslefree.svg" className="m-1 mb-3 img-fluid" style={{ width: "35px" }} alt="" />
+                        <h6 className="display-6" style={{ fontSize: "0.9rem" }}>Hassle free Loan</h6>
+                        <p className="display-6" style={{ fontSize: "0.6rem" }}>No visit to the branch is required</p>
+                    </div>
+                    <div className="col-6 col-lg-4 text-start p-3">
+                        <img src="WhyPowerCash/safe.svg" className="m-1 mb-3 img-fluid" style={{ width: "38px" }} alt="" />
+                        <h6 className="display-6" style={{ fontSize: "0.9rem" }}>Safe and Secure</h6>
+                        <p className="display-6" style={{ fontSize: "0.6rem" }}>An ISO-certified company keeps
+                            your data safe</p>
+                    </div>
+                    <div className="col-6 col-lg-4 text-start p-3">
+                        <img src="WhyPowerCash/lowint.svg" className="m-1 mb-3 img-fluid" style={{ width: "35px" }} alt="" />
+                        <h6 className="display-6" style={{ fontSize: "0.9rem" }}>Low-Interest Rates</h6>
+                        <p className="display-6" style={{ fontSize: "0.6rem" }}>Enjoy affordable interest rates that
+                            suit your needs</p>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+    )
+}
